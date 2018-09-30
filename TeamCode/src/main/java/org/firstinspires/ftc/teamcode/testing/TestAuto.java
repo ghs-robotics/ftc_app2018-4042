9076eb120bfea4abcb20818406bd0f4a1ff8543c
AlnisS <alnis.smidchens@gmail.com>
2018-09-29 20:36:53 -0700

    test auto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testing/TestAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testing/TestAuto.java
@@ -4,6 +4,9 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 
 import org.firstinspires.ftc.teamcode.core.ClassHolder;
 import org.firstinspires.ftc.teamcode.core.OpModeExtended;
+import org.firstinspires.ftc.teamcode.core.Registry;
+import org.firstinspires.ftc.teamcode.core.SensorManager;
+import org.firstinspires.ftc.teamcode.core.Subsystem;
 
 @Autonomous(name = "Test", group = "Tests")
 public class TestAuto extends OpModeExtended {
@@ -21,7 +24,9 @@ public class TestAuto extends OpModeExtended {
             file = null;
         }
         public void autoupdate() {
-
+            Subsystem subsystem = Registry.getSubsystemByName("fakeLoggingSubsystem");
+            SensorManager sensorManager = Registry.getSensorManagerByName("fakeTimeSensor");
+            subsystem.setting("log: test", "" + sensorManager.getCM());
         }
     }
 }
