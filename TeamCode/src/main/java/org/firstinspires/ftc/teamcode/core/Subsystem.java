8441c034b909bada2489fdccafaed326e9a222a4
AlnisS <alnis.smidchens@gmail.com>
2018-10-01 20:28:33 -0700

    subsystem settings are Objects

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/core/Subsystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/core/Subsystem.java
@@ -4,36 +4,15 @@ import java.util.HashMap;
 import java.util.Map;
 
 public abstract class Subsystem {
-    public Map<String, Integer> integers = new HashMap<>();
-    public Map<String, Double> doubles = new HashMap<>();
-    public Map<String, Boolean> booleans = new HashMap<>();
-    public Map<String, String> strings = new HashMap<>();
+    public Map<String, Object> settings = new HashMap<>();
 
     public abstract void init();
     public abstract void updateData();
     public abstract void updateActuators();
-    public void setting(String name, int value) {
-        integers.put(name, value);
+    public void setting(String name, Object object) {
+        settings.put(name, object);
     }
-    public void setting(String name, double value) {
-        doubles.put(name, value);
-    }
-    public void setting(String name, boolean value) {
-        booleans.put(name, value);
-    }
-    public void setting(String name, String value) {
-        strings.put(name, value);
-    }
-    public int getIntegerSetting(String name) {
-        return integers.get(name);
-    }
-    public double getDoubleSetting(String name) {
-        return doubles.get(name);
-    }
-    public boolean getBooleanSetting(String name) {
-        return booleans.get(name);
-    }
-    public String getStringSetting(String name) {
-        return strings.get(name);
+    public Object getObjectSetting(String name) {
+        return settings.get(name);
     }
 }
