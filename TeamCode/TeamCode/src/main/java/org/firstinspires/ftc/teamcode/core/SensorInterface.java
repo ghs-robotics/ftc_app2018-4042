a107b33cf43e980880b31e7dbf68861f4388048f
Ottomated <otto@ottomated.pw>
2018-10-30 17:43:27 -0700

    Fixed Moses' sh***y copying job by gradle magic

--- TeamCode/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/core/SensorInterface.java
+++
@@ -1,9 +0,0 @@
-package org.firstinspires.ftc.teamcode.core;
-
-public interface SensorInterface {
-    void init();
-    void update();
-    double getCMValue();
-    double getRawValue();
-    String getConfigName();
-}
