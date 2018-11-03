6790b4f360a7fd2f88a6ffa4426e0b6c6e6cf2b0
Moses Miller <Majora320@gmail.com>
2018-11-03 16:10:40 -0700

    Implement sensor interface to Tealisp

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testing/TestHolder.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testing/TestHolder.java
@@ -2,9 +2,12 @@ package org.firstinspires.ftc.teamcode.testing;
 
 import org.firstinspires.ftc.teamcode.core.ClassHolder;
 import org.firstinspires.ftc.teamcode.core.OpModeExtended;
+import org.firstinspires.ftc.teamcode.core.SensorInterface;
 import org.firstinspires.ftc.teamcode.core.SensorManager;
 import org.firstinspires.ftc.teamcode.core.Subsystem;
 
+import org.firstinspires.ftc.teamcode.core.tealisp.TealispSensorInterface;
+import org.firstinspires.ftc.teamcode.core.tealisp.TealispSubsystemInterface;
 import org.majora320.tealisp.evaluator.JavaInterface;
 
 import java.util.HashMap;
@@ -22,6 +25,7 @@ public class TestHolder extends ClassHolder {
         Map<String, Subsystem> result = new HashMap<>();
 
         result.put("fakeLoggingSubsystem", new FakeLoggingSubsystem(context));
+        result.put("testServo", new TestServo(context, "servo"));
 
         return result;
     }
@@ -38,6 +42,9 @@ public class TestHolder extends ClassHolder {
 
     @Override
     public Map<String, JavaInterface> getInterfaces() {
-        return new HashMap<>();
+        Map<String, JavaInterface> res = new HashMap<>();
+        res.put("subsystems", new TealispSubsystemInterface());
+        res.put("sensors", new TealispSensorInterface());
+        return res;
     }
 }
