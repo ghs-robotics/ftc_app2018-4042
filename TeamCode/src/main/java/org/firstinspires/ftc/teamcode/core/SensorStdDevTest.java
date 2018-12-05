5123d9baf124fd9565407e4cdd437dc31efe01b2
gpsanant <gpsanant@gmail.com>
2018-12-04 17:31:57 -0800

    sigma1

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/core/SensorStdDevTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/core/SensorStdDevTest.java
@@ -7,9 +7,10 @@ import com.qualcomm.robotcore.hardware.AnalogInput;
 import java.util.ArrayList;
 
 @TeleOp(name="SensorStdDevTest", group="Iterative Opmode")
+// This opmode gets the standard deviation of a sensor by
+// constantly replacing voltages in a list consisting of 100 values
 public class SensorStdDevTest extends OpMode {
     static final int NUM_VOLTAGES = 100;
-
     AnalogInput ultrasonic;
     ArrayList<Double> voltageList;
     double sum;
@@ -24,12 +25,10 @@ public class SensorStdDevTest extends OpMode {
      @Override
      public void loop() {
          sum = 0;
-         if(voltageList.size() < NUM_VOLTAGES){
-             voltageList.add(ultrasonic.getVoltage());
-         }else{
+         voltageList.add(ultrasonic.getVoltage());
+         if(voltageList.size() > NUM_VOLTAGES)
              voltageList.remove(0);
-             voltageList.add(ultrasonic.getVoltage());
-         }
+
          for(double voltage : voltageList){
              sum += voltage;
          }
@@ -37,7 +36,7 @@ public class SensorStdDevTest extends OpMode {
          for(double voltage : voltageList){
              sum += Math.pow((voltage - mean), 2);
          }
-         stdDev = Math.sqrt(sum/voltageList.size()-1);
+         stdDev = Math.sqrt(sum/(voltageList.size()-1));
          telemetry.addData("hi brendan", stdDev);
          telemetry.update();
      }
